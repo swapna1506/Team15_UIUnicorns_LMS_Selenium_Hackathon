0ddb8c8f3b759a302c479f25deb498bd2cb7f92c
Pooja Tavag <poojatavag1@gmail.com>
2023-09-13 23:18:28 -0400

    Pooja's Pgm Module

--- src/test/java/com/Utils/BaseClass.java
+++ src/test/java/com/Utils/BaseClass.java
@@ -1,5 +1,26 @@
 package com.Utils;
 
+import org.openqa.selenium.WebDriver;
+
+import static com.Utils.Helper.getDriver;
+import com.PageObjects.*;
+
 public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	public static ConfigReader cr = new ConfigReader();
+	
+	public static LoginPage lp;
+	public static DashboardPage dp;
+	public static DeletePgmConfirmPopupPage dpp;
+	public static ManagePgmsPage mpp;
+	public static EditProgramDetailsPopupPage epp;
+	
+
+	
+	static {
+		driver = getDriver();
+	}
 
 }
